ef1bc719e44aa3ff60c6ff915872ccf79816be4d
Aziza <azizamukashkyzy@gmail.com>
2023-04-07 17:29:42 +0600

    Модель NewOrder (Новый образец),DailyWork(Ежедневник)

--- factory/models.py
+++ factory/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from employees import Employee
+from client import Client
 
 # Create your models here.
 
@@ -28,3 +30,36 @@ class Order(models.Model):
 
     def __str__(self) -> str:
         return f'Имя клиента {self.client}. Имя продутка {self.name_order}.'
+
+
+class DailyWork(models.Model):
+    employee = models.ForeignKey(
+        Employee, on_delete=models.CASCADE, verbose_name="Cотрудник")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name="Модель")  # модель
+    quantity = models.PositiveIntegerField(verbose_name="Количество")
+    date = models.DateField(auto_now_add=True, verbose_name="Дата")
+    prepayment = models.IntegerField(default=0, verbose_name="Аванс")
+
+    def __str__(self):
+        return self.date
+
+    class Meta:
+        verbose_name = "Ежедневник"
+        verbose_name_plural = "Ежедневники"
+
+
+class NewOrder(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name="Модель")
+    price = models.PositiveIntegerField()
+    color = models.CharField(max_length=25)
+    image = models.ImageField(null=True, blank=True)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, verbose_name="Клиент")
+    received_date = models.DateField(verbose_name="Дата получения")
+    delivery_date = models.DateField(verbose_name="Дата отправки")
+
+    def __str__(self):
+        return self.product
+
+    class Meta:
+        verbose_name = "Образец"
+        verbose_name_plural = "Образцы"
