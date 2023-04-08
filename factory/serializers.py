ce3fe8f96346eb2ff60163d300224fb3089d6433
Aziza <azizamukashkyzy@gmail.com>
2023-04-08 13:12:12 +0600

    Модель NewOrder, DailyWork

--- factory/serializers.py
+++ factory/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
-from .models import Order
+from .models import (Order,
+                     NewOrder,
+                     DailyWork)
+
 
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = '__all__'
-        
+
+
+class NewOrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NewOrder
+        fields = '__all__'
