17f0798dc0b61bc7bea228c367e9a6d3c3af4de2
Maksat-developer <gangsta.okulus02@.com>
2023-04-09 12:06:36 +0600

    2.2.1.1. Сериалайзер для SewingModel/2.2.1 CRUD SewingModel (модель для пошива)/2.2.1.2 прописать urlpatterns для SewingModel

--- factory/serializers.py
+++ factory/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import Order
+from .models import Order, SewingModel
+
+
+class SewingModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SewingModel
+        fieds = "__all__"
+
 
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
-        fields = '__all__'
-        
+        fields = "__all__"
