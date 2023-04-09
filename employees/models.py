cd183fa6f0761f2f1596c7a0db6668968ba46d90
yulia-illiriya <yulia.illiriya@gmail.com>
2023-04-09 13:03:14 +0600

    added Employee model

--- employees/models.py
+++ employees/models.py
@@ -11,3 +11,17 @@ class Position(models.Model):
         verbose_name = "Должность"
         verbose_name_plural = "Должности"
     
+
+class Employee(models.Model):
+    full_name = models.CharField("ФИО", max_length=120)
+    contacts = models.CharField("Контакты", max_length=200)
+    start_date = models.DateTimeField("Дата начала работы")
+    position = models.ForeignKey(Position, on_delete=models.CASCADE, verbose_name="Должность")
+    salary = models.DecimalField("Оклад", default=0)
+    
+    def __str__(self):
+        return self.full_name, self.position
+    
+    class Meta:
+        verbose_name = "Сотрудник"
+        verbose_name_plural = "Сотрудники"
