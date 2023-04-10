a41bb6cac04bc161cbde2434f03152f7140bb9cf
Maksat-developer <gangsta.okulus02@.com>
2023-04-10 16:53:17 +0600

    fix/bag

--- config/settings.py
+++ config/settings.py
@@ -76,17 +76,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'limongroup',
-        'USER': 'aibek',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+DATABASES = {"default": env.db()}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
