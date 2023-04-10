e525c062da94926d1c8a5d264573d7bb7ac8d2a6
yulia-illiriya <yulia.illiriya@gmail.com>
2023-04-10 15:06:14 +0600

    added CRUD for model Client

--- client/views.py
+++ client/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics
+from client.models import Client
+from client.serializers import ClientSerializer
 
-# Create your views here.
+class ClientListCreateAPIViews(generics.ListCreateAPIView):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+    
+class ClientDetailUpdateAPIView(generics.RetrieveUpdateAPIView):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+    
+class ClientReadDeleteAPIView(generics.RetrieveDestroyAPIView):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+    
