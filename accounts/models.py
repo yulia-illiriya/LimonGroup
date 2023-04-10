8898826dd113c30bba58f421dc5aec28bc87adf1
Adeliya62477 <adelyatalantovaa@gmail.com>
2023-04-10 13:13:50 +0600

    accounts_changes_models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+
+class User(AbstractBaseUser, PermissionsMixin):
+    POSITION_CHOICES = (
+        ('Administrator', 'Administrator'),
+        ('Technologist', 'Technologist'),
+        ('Zakroi', 'Zakroi')
+    )
+
+    position = models.CharField(max_length=50, choices=POSITION_CHOICES)
+    email = models.EmailField(
+        verbose_name="Email",
+        max_length=255,
+        unique=True,
+    )
+    first_name = models.CharField(max_length=255, verbose_name='Имя')
+    last_name = models.CharField(max_length=255, verbose_name='Фамилия')
+    date_created = models.DateField(auto_now_add=True)
+    date_updated = models.DateField(auto_now=True)
