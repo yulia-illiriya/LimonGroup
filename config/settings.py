2a9ecd51b6df50a37e3027661c7ca48e574a760c
Adeliya62477 <adelyatalantovaa@gmail.com>
2023-04-10 17:41:57 +0600

    settings.py/connect db

--- config/settings.py
+++ config/settings.py
@@ -76,6 +76,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
