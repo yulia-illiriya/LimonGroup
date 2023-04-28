907cc24bb899eb00953847a157d9fb7e2e6033a2
AibekYrysbekov <yrysbekovayba@gmail.com>
2023-04-28 16:38:04 +0600

    clean trash

--- config/settings.py
+++ config/settings.py
@@ -80,7 +80,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
-DATABASES = {"default": env.db()}
+# DATABASES = {"default": env.db()}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
