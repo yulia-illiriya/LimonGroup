d7ca3099aab4ea2883517238115448ddc1ec97d6
Maksat-developer <gangsta.okulus02@gmail.com>
2023-06-02 17:28:15 +0600

    change/accounts/urls.py/accounts/views.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include, re_path
 from rest_framework.routers import SimpleRouter
 from djoser.views import UserViewSet
-from .views import UserCreateView
+from .views import UserCreateView, CustomAuthToken, TestMeAPIView
 
 # from accounts.views import UserViewSet, UserMeAPIView
 from djoser import views
@@ -13,8 +13,9 @@ from djoser import views
 
 urlpatterns = [
     path('register/', UserCreateView.as_view({'post': 'create'}), name='register'),
+    path('auth/token/', CustomAuthToken.as_view(), name='auth_token'),
 #     path('auth/', include('djoser.urls')),
 #     re_path(r'^auth/', include('djoser.urls.authtoken'))
-# #     path('users/me/', UserMeAPIView.as_view(), name='me'),
+    # path('users/me/', TestMeAPIView.as_view(), name='me'),
 ]
 # # urlpatterns += router.urls
--- accounts/views.py
+++ accounts/views.py
@@ -13,6 +13,29 @@ from accounts.models import User
 from .serializers import UserAPISerializer, UserCreateAPISerializer
 
 
+from djoser import views as djoser_views
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+
+
+class CustomAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key})
+
+
 class UserCreateView(DjoserUserViewSet):
     """
     Custom view for user registration using Djoser.
@@ -41,3 +64,17 @@ class UserMeAPIView(RetrieveAPIView):
 
         user = self.get_serializer(request.user).data
         return Response(data=user)
+
+class TestMeAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    @swagger_auto_schema(tags=['users'], manual_parameters=[
+        openapi.Parameter('Authorization', openapi.IN_HEADER, type=openapi.TYPE_STRING, description='Token'),
+    ])
+    def get(self, request):
+        if not request.user.is_authenticated:
+            return Response(data='Not authenticated', status=status.HTTP_401_UNAUTHORIZED)
+
+        user = request.user
+        serializer = UserAPISerializer(user)
+        return Response(serializer.data)
